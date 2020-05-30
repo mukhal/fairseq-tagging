c90dd126e0c143ee775adbe34043aca928a1ea89
muhammad.e.khalifa@gmail.com <muhammad.e.khalifa@gmail.com>
2020-05-30 20:36:45 +0200

    added bpeing sources and targets

--- fairseq_cli/preprocess.py
+++ fairseq_cli/preprocess.py
@@ -28,7 +28,8 @@ logging.basicConfig(
 )
 logger = logging.getLogger('fairseq_cli.preprocess')
 
-def convert_seqtag_format_to_src_tgt(files_dir):
+
+def convert_seqtag_format_to_src_tgt(files_dir, split):
     """
     Converts earch sequence tagging file to a source and target format. 
     For example :
@@ -43,66 +44,109 @@ def convert_seqtag_format_to_src_tgt(files_dir):
         *.source:
             West Indian all-rounder Phil Simmons .
         and *.target :
-            B-MISC I-MISC O B-PER I PER O
+            B-MISC I-MISC O B-PER I-PER O
 
     """
 
     out_dir = os.path.join(files_dir, "fseq-outputs")
-    
+
     # create output directory if not exists
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
 
-    for split in ['train', 'valid', 'test']:
-        path = os.path.join(files_dir, split)
-        
-        if not os.path.exists(path):
-            raise FileExistsError(path)
-        
-        f = open(path)
-        data = []
+    path = os.path.join(files_dir, split)
+
+    if not os.path.exists(path):
+        return None  # file does not exist
+
+    f = open(path)
+    data = []
+    sentence = []
+    label = []
+
+    for line in f:
+        if not line.strip() or len(line) == 0 or line.startswith('-DOCSTART') or line[0] == "\n" or line[0] == '.':
+            if len(sentence) > 0:
+                data.append((sentence, label))
+                sentence = []
+                label = []
+            continue
+
+        splits = line.split()
+        word, tag = splits[0], splits[-1]
+        sentence.append(word.strip())
+        label.append(tag.strip())
+
+    if len(sentence) > 0:
+        data.append((sentence, label))
         sentence = []
         label = []
-        
-        for line in f:
-            if not line.strip() or len(line) == 0 or line.startswith('-DOCSTART') or line[0] == "\n" or line[0] == '.':
-                if len(sentence) > 0:
-                    data.append((sentence, label))
-                    sentence = []
-                    label = []
-                continue
 
-            splits = line.split()
-            word, tag = splits[0], splits[-1]
-            sentence.append(word.strip())
-            label.append(tag.strip())
+    # write output
+    outsrc_path = os.path.join(out_dir, '{}.source'.format(split))
+    outtgt_path = os.path.join(out_dir, '{}.target'.format(split))
+    f_outsrc = open(outsrc_path, 'w')
+    f_outtgt = open(outtgt_path, 'w')
 
-        if len(sentence) > 0:
-            data.append((sentence, label))
-            sentence = []
-            label = []
-        
+    for src, tgt in data:
+        assert len(src) == len(tgt)
+        f_outsrc.write(' '.join(src) + '\n')
+        f_outtgt.write(' '.join(tgt) + '\n')
 
-        # write output
-        outsrc_path = os.path.join(out_dir, '{}.source'.format(split))
-        outtgt_path = os.path.join(out_dir, '{}.target'.format(split))
-        f_outsrc = open(outsrc_path, 'w')
-        f_outtgt = open(outtgt_path, 'w')
+    logger.info("Wrote source, target to {} {}".format(
+        outsrc_path, outtgt_path))
+    return os.path.join(out_dir, split)  # return split path prefix
 
-        for src, tgt in data:
-            assert len(src) == len(tgt)
-            f_outsrc.write(' '.join(src)+ '\n')
-            f_outtgt.write(' '.join(tgt) + '\n')
 
-        logger.info("Wrote source, target to {} {}".format(outsrc_path, outtgt_path))
-    return out_dir
-        
+def create_bpe(path_pref, spm_model):
+    """
+    Takes a split path prefix with .source and .target file, encodes source using BPE, modifies target to tag first token for each word
 
+    For example, the source, target pair: 
+    West Indian all-rounder Phil Simmons . -----> B-MISC I-MISC O B-PER I-PER O
+
+    is converted to:
+    (We _st In _dian all _- round _er _Ph _il _sim _mons _. -----> B_MISC <pad> I-MISC <pad> O <pad> <pad> <pad> <pad> B-PER <pad> I-PER <pad> O)
+
+    Args:
+        path_pref (str): path prefix of this split.
+        spm_model: directory to the trained sentencepiece model
+
+    """
+
+    import sentencepiece as spm
+    sp = spm.SentencePieceProcessor()
+    sp.Load(spm_model)
+
+    def encode(l):
+        return sp.EncodeAsPieces(l)
+
+    # input files
+    f_source = open(path_pref + '.source', encoding='utf-8')
+    f_target = open(path_pref + '.target', encoding='utf-8')
+
+    # bpe out files
+    f_source_out = open(path_pref + '.source.bpe', 'w+', encoding='utf-8')
+    f_target_out = open(path_pref + '.target.bpe', 'w+', encoding='utf-8')
+
+    for s, t in zip(f_source, f_target):
+        cur_source = []
+        cur_target = []
+        for word, tag in zip(s.split(), t.split()):
+            enc_word = encode(word)
+            bpe_tags = [tag] + ['<pad>'] * (len(enc_word) - 1)
+
+            cur_source.extend(enc_word)
+            cur_target.extend(bpe_tags)
+
+        assert len(cur_source) == len(cur_target)
+
+        f_source_out.write(' '.join(cur_source) + '\n')
+        f_target_out.write(' '.join(cur_target) + '\n')
 
 
 def main(args):
     utils.import_user_module(args)
-
     os.makedirs(args.destdir, exist_ok=True)
 
     logger.addHandler(logging.FileHandler(
@@ -114,14 +158,30 @@ def main(args):
 
     # TODO: check base if task == seqtag
     assert args.seqtag_data_dir is not None, "you must provide directory for original sequence tagging data"
-    src_tgt_dir = convert_seqtag_format_to_src_tgt(args.seqtag_data_dir)
-    
-    args.trainpref = os.path.join(src_tgt_dir, 'train')
-    args.validpref = os.path.join(src_tgt_dir, 'valid')
-    args.testpref = os.path.join(src_tgt_dir, 'test')
+
+    args.trainpref = convert_seqtag_format_to_src_tgt(
+        args.seqtag_data_dir, split='train')
+    args.validpref = convert_seqtag_format_to_src_tgt(
+        args.seqtag_data_dir, split='valid')
+    args.testpref = convert_seqtag_format_to_src_tgt(
+        args.seqtag_data_dir, split='test')
 
     args.source_lang = 'source'
     args.target_lang = 'target'
+
+    if args.bpe:
+        logger.info("BPEing data...")
+        assert args.bpe in [
+            'sentencepiece'], "not supported BPE method :{}".format(args.bpe)
+        assert args.bpe != 'sentencepiece' or args.sentencepiece_model is not None, "you must specify directory for the sentencepiece model"
+
+        # encode bpe
+        create_bpe(args.trainpref, args.sentencepiece_model)
+        create_bpe(args.validpref, args.sentencepiece_model)
+        create_bpe(args.testpref, args.sentencepiece_model)
+
+        args.source_lang = 'source.bpe'
+        args.target_lang = 'target.bpe'
 
     def train_path(lang):
         return "{}{}".format(args.trainpref, ("." + lang) if lang else "")
@@ -143,9 +203,9 @@ def main(args):
         return task.build_dictionary(
             filenames,
             workers=args.workers,
-            threshold=args.thresholdsrc if src else args.thresholdtgt,
-            nwords=args.nwordssrc if src else args.nwordstgt,
-            padding_factor=args.padding_factor,
+            threshold=args.thresholdsrc if src else 0,
+            nwords=args.nwordssrc if src else -1,
+            padding_factor=1,
         )
 
     target = not args.only_source
@@ -174,14 +234,16 @@ def main(args):
             src_dict = task.load_dictionary(args.srcdict)
         else:
             assert args.trainpref, "--trainpref must be set if --srcdict is not specified"
-            src_dict = build_dictionary([train_path(args.source_lang)], src=True)
+            src_dict = build_dictionary(
+                [train_path(args.source_lang)], src=True)
 
         if target:
             if args.tgtdict:
                 tgt_dict = task.load_dictionary(args.tgtdict)
             else:
                 assert args.trainpref, "--trainpref must be set if --tgtdict is not specified"
-                tgt_dict = build_dictionary([train_path(args.target_lang)], tgt=True)
+                tgt_dict = build_dictionary(
+                    [train_path(args.target_lang)], tgt=True)
         else:
             tgt_dict = None
 
@@ -311,32 +373,40 @@ def main(args):
         if args.dataset_impl == "raw":
             # Copy original text file to destination folder
             output_text_file = dest_path(
-                output_prefix + ".{}-{}".format(args.source_lang, args.target_lang),
+                output_prefix +
+                ".{}-{}".format(args.source_lang, args.target_lang),
                 lang,
             )
             shutil.copyfile(file_name(input_prefix, lang), output_text_file)
         else:
-            make_binary_dataset(vocab, input_prefix, output_prefix, lang, num_workers)
+            make_binary_dataset(vocab, input_prefix,
+                                output_prefix, lang, num_workers)
 
     def make_all(lang, vocab):
         if args.trainpref:
-            make_dataset(vocab, args.trainpref, "train", lang, num_workers=args.workers)
+            make_dataset(vocab, args.trainpref, "train",
+                         lang, num_workers=args.workers)
         if args.validpref:
             for k, validpref in enumerate(args.validpref.split(",")):
                 outprefix = "valid{}".format(k) if k > 0 else "valid"
-                make_dataset(vocab, validpref, outprefix, lang, num_workers=args.workers)
+                make_dataset(vocab, validpref, outprefix,
+                             lang, num_workers=args.workers)
         if args.testpref:
             for k, testpref in enumerate(args.testpref.split(",")):
                 outprefix = "test{}".format(k) if k > 0 else "test"
-                make_dataset(vocab, testpref, outprefix, lang, num_workers=args.workers)
+                make_dataset(vocab, testpref, outprefix,
+                             lang, num_workers=args.workers)
 
     def make_all_alignments():
         if args.trainpref and os.path.exists(args.trainpref + "." + args.align_suffix):
-            make_binary_alignment_dataset(args.trainpref + "." + args.align_suffix, "train.align", num_workers=args.workers)
+            make_binary_alignment_dataset(
+                args.trainpref + "." + args.align_suffix, "train.align", num_workers=args.workers)
         if args.validpref and os.path.exists(args.validpref + "." + args.align_suffix):
-            make_binary_alignment_dataset(args.validpref + "." + args.align_suffix, "valid.align", num_workers=args.workers)
+            make_binary_alignment_dataset(
+                args.validpref + "." + args.align_suffix, "valid.align", num_workers=args.workers)
         if args.testpref and os.path.exists(args.testpref + "." + args.align_suffix):
-            make_binary_alignment_dataset(args.testpref + "." + args.align_suffix, "test.align", num_workers=args.workers)
+            make_binary_alignment_dataset(
+                args.testpref + "." + args.align_suffix, "test.align", num_workers=args.workers)
 
     make_all(args.source_lang, src_dict)
     if target:
@@ -357,7 +427,8 @@ def main(args):
                     for a, s, t in zip_longest(align_file, src_file, tgt_file):
                         si = src_dict.encode_line(s, add_if_not_exist=False)
                         ti = tgt_dict.encode_line(t, add_if_not_exist=False)
-                        ai = list(map(lambda x: tuple(x.split("-")), a.split()))
+                        ai = list(
+                            map(lambda x: tuple(x.split("-")), a.split()))
                         for sai, tai in ai:
                             srcidx = si[int(sai)]
                             tgtidx = ti[int(tai)]
@@ -376,12 +447,14 @@ def main(args):
 
         align_dict = {}
         for srcidx in freq_map.keys():
-            align_dict[srcidx] = max(freq_map[srcidx], key=freq_map[srcidx].get)
+            align_dict[srcidx] = max(
+                freq_map[srcidx], key=freq_map[srcidx].get)
 
         with open(
                 os.path.join(
                     args.destdir,
-                    "alignment.{}-{}.txt".format(args.source_lang, args.target_lang),
+                    "alignment.{}-{}.txt".format(args.source_lang,
+                                                 args.target_lang),
                 ),
                 "w", encoding='utf-8'
         ) as f:
@@ -389,7 +462,7 @@ def main(args):
                 print("{} {}".format(src_dict[k], tgt_dict[v]), file=f)
 
 
-def binarize(args, filename, vocab, output_prefix, lang, offset, end, append_eos=True):
+def binarize(args, filename, vocab, output_prefix, lang, offset, end, append_eos=False):
     ds = indexed_dataset.make_builder(dataset_dest_file(args, output_prefix, lang, "bin"),
                                       impl=args.dataset_impl, vocab_size=len(vocab))
 
@@ -398,6 +471,7 @@ def binarize(args, filename, vocab, output_prefix, lang, offset, end, append_eos
 
     res = Binarizer.binarize(filename, vocab, consumer, append_eos=append_eos,
                              offset=offset, end=end)
+
     ds.finalize(dataset_dest_file(args, output_prefix, lang, "idx"))
     return res
 
@@ -418,7 +492,8 @@ def binarize_alignments(args, filename, parse_alignment, output_prefix, offset, 
 def dataset_dest_prefix(args, output_prefix, lang):
     base = "{}/{}".format(args.destdir, output_prefix)
     if lang is not None:
-        lang_part = ".{}-{}.{}".format(args.source_lang, args.target_lang, lang)
+        lang_part = ".{}-{}.{}".format(args.source_lang,
+                                       args.target_lang, lang)
     elif args.only_source:
         lang_part = ""
     else:
@@ -438,7 +513,11 @@ def get_offsets(input_file, num_workers):
 
 def cli_main():
     parser = options.get_preprocessing_parser()
-    parser.add_argument('--seqtag-data-dir',default=None, type=str)
+    parser.add_argument('--seqtag-data-dir', default=None,
+                        type=str, help='directory for IOB formatted data')
+    parser.add_argument('--sentencepiece-model', type=str,
+                        default=None, help='directorty for the sentencepiece model')
+
     args = parser.parse_args()
     main(args)
 
