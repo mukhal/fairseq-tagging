f6e9732d7664b83bd7e5eb3a40dc0c4f7e4b6035
muhammad.e.khalifa@gmail.com <muhammad.e.khalifa@gmail.com>
2020-05-31 19:08:14 +0200

    prediction script

---
+++ fairseq_cli/predict.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3 -u
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+"""
+Translate pre-processed data with a trained model.
+"""
+
+import logging
+import math
+import os
+import sys
+
+import torch
+
+from fairseq import bleu, checkpoint_utils, options, tasks, utils
+from fairseq.logging import progress_bar
+from fairseq.logging.meters import StopwatchMeter, TimeMeter
+from fairseq.data import encoders
+
+
+def main(args):
+
+    assert args.path is not None, '--path required for generation!'
+    assert not args.sampling or args.nbest == args.beam, \
+        '--sampling requires --nbest to be equal to --beam'
+    assert args.replace_unk is None or args.dataset_impl == 'raw', \
+        '--replace-unk requires a raw text dataset (--dataset-impl=raw)'
+
+    if args.results_path is not None:
+        os.makedirs(args.results_path, exist_ok=True)
+        output_path = os.path.join(args.results_path, 'generate-{}.txt'.format(args.gen_subset))
+        with open(output_path, 'w', buffering=1) as h:
+            return _main(args, h)
+    else:
+        return _main(args, sys.stdout)
+
+
+def _main(args, output_file):
+    logging.basicConfig(
+        format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.INFO,
+        stream=output_file,
+    )
+    logger = logging.getLogger('fairseq_cli.generate')
+
+    utils.import_user_module(args)
+
+    if args.max_tokens is None and args.max_sentences is None:
+        args.max_tokens = 12000
+    logger.info(args)
+
+    use_cuda = torch.cuda.is_available() and not args.cpu
+
+    # Load dataset splits
+    task = tasks.setup_task(args)
+    task.load_dataset(args.gen_subset)
+
+    # Set dictionaries
+    try:
+        src_dict = getattr(task, 'source_dictionary', None)
+    except NotImplementedError:
+        src_dict = None
+    tgt_dict = task.target_dictionary
+
+    # Load ensemble
+    logger.info('loading model(s) from {}'.format(args.path))
+    models, _model_args = checkpoint_utils.load_model_ensemble(
+        utils.split_paths(args.path),
+        arg_overrides=eval(args.model_overrides),
+        task=task,
+    )
+
+    # Optimize ensemble for generation
+    for model in models:
+        if args.fp16:
+            model.half()
+        if use_cuda:
+            model.cuda()
+
+    # Load dataset (possibly sharded)
+    itr = task.get_batch_iterator(
+        dataset=task.dataset(args.gen_subset),
+        max_tokens=args.max_tokens,
+        max_sentences=args.max_sentences,
+        max_positions=utils.resolve_max_positions(
+            task.max_positions(),
+            *[model.max_positions() for model in models]
+        ),
+        ignore_invalid_inputs=args.skip_invalid_size_inputs_valid_test,
+        required_batch_size_multiple=args.required_batch_size_multiple,
+        num_shards=args.num_shards,
+        shard_id=args.shard_id,
+        num_workers=args.num_workers,
+    ).next_epoch_itr(shuffle=False)
+    progress = progress_bar.progress_bar(
+        itr,
+        log_format=args.log_format,
+        log_interval=args.log_interval,
+        default_log_format=('tqdm' if not args.no_progress_bar else 'none'),
+    )
+
+    # Initialize generator
+    gen_timer = StopwatchMeter()
+
+    # Handle tokenization and BPE
+    tokenizer = encoders.build_tokenizer(args)
+    bpe = encoders.build_bpe(args)
+
+    def decode_fn(x):
+        if bpe is not None:
+            x = bpe.decode(x)
+        if tokenizer is not None:
+            x = tokenizer.decode(x)
+        return x
+
+
+    for sample in progress:
+        sample = utils.move_to_cuda(sample) if use_cuda else sample
+        if 'net_input' not in sample:
+            continue
+
+        if args.prefix_size > 0:
+            prefix_tokens = sample['target'][:, :args.prefix_size]
+
+
+        for i, sample_id in enumerate(sample['id'].tolist()):
+            has_target = sample['target'] is not None
+
+            # Remove padding
+            src_tokens = utils.strip_pad(sample['net_input']['src_tokens'][i, :], tgt_dict.pad())
+            target_tokens = None
+            if has_target:
+                target_tokens = utils.strip_pad(sample['target'][i, :], tgt_dict.pad()).int().cpu()
+
+            # Either retrieve the original sentences or regenerate them from tokens.
+            if src_dict is not None:
+                src_str = src_dict.string(src_tokens, args.remove_bpe)
+            else:
+                src_str = ""
+
+            print(src_str)
+
+            if has_target:
+                target_str = tgt_dict.string(
+                    target_tokens,
+                    args.remove_bpe,
+                    escape_unk=True,
+                )
+
+            src_str = decode_fn(src_str)
+            if has_target:
+                target_str = decode_fn(target_str)
+
+            if not args.quiet:
+                if src_dict is not None:
+                    print('S-{}\t{}'.format(sample_id, src_str), file=output_file)
+                if has_target:
+                    print('T-{}\t{}'.format(sample_id, target_str), file=output_file)
+
+            
+
+
+
+def cli_main():
+    parser = options.get_generation_parser()
+    args = options.parse_args_and_arch(parser)
+    main(args)
+
+
+if __name__ == '__main__':
+    cli_main()
